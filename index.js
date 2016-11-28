f6bfeb399033632d92d4861ceae21afd15868342
Elizabeth Wicks <elizabeth.wicks@hotmail.com>
2016-11-28 15:05:12 +0000

    takes in key

--- index.js
+++ index.js
@@ -3,23 +3,25 @@ var app = express()
 var bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function(req, res){
   res.send('Hello World')
 });
 
 
-app.get('/set?', function(req, res){
-  req.json = ({ key: 'value'})
+app.get('/set*', function(req, res){
+  var key = req.query.key;
 
 
+  res.send(key)
 });
 
-
-
+app.get('/get?', function(req, res){
+  res.json({ key : req.params.key,
+            value: req.query.value});
+});
 
 app.listen(3000, function(){
   console.log("started on 3000")
-
 });
